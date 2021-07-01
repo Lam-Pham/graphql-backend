ba9610520e252fbe1672429e9c1ff131d2e91962
Lam-Pham <lpham127@gmail.com>
2021-06-30 17:59:09 -0700

    exercise 8.2: added allBooks query

--- index.js
+++ index.js
@@ -85,16 +85,25 @@ let books = [
 ]
 
 const typeDefs = gql`
-  type Query {
+    type Book {
+        title: String!
+        published: Int!
+        author: String!
+        genres: [String!]!
+    }
+
+    type Query {
       bookCount: Int!
       authorCount: Int!
-  }
+      allBooks: [Book!]!
+    }
 `
 
 const resolvers = {
   Query: {
       bookCount: () => books.length,
-      authorCount: () => authors.length
+      authorCount: () => authors.length,
+      allBooks: () => books
   }
 }
 
